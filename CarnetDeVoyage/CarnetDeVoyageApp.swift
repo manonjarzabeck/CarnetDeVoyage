9cdc1d17f12748dae62d7602e72a7802ccdfd4a6
manonjarzabeck <manonjarzabeck@gmail.com>
2026-06-01 13:50:33 +0200

    Initial Commit

---
+++ CarnetDeVoyage/CarnetDeVoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarnetDeVoyageApp.swift
+//  CarnetDeVoyage
+//
+//  Created by Manon  on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CarnetDeVoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
