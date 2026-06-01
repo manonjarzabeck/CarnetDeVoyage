f4d5ace5412c143ee0a2bc78e8115e7722d36c5e
manonjarzabeck <manonjarzabeck@gmail.com>
2026-06-01 14:33:02 +0200

    Title added 🏔️

--- CarnetDeVoyage/ContentView.swift
+++ CarnetDeVoyage/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "moon.stars")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Welcome to my app 🏔️")
         }
         .padding()
     }
